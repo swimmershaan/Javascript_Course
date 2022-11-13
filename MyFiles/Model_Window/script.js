4a20f6b311ba2c8f2a43bfe14a7fc2d440817e13
swimmershaan <swimmershaan@gmail.com>
2022-11-13 01:51:48 -0800

    Modal Windows

--- MyFiles/Model_Window/script.js
+++ MyFiles/Model_Window/script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+// Define Elements
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+// Open Modal
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener("click", openModal);
+
+// Close Modal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
